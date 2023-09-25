ffca2ac60cd8ba41a1e7500eae2f817f4e2db226
Mingi Kim <migni4575@gmail.com>
2023-09-25 13:28:16 +0900

    # feat: Nest.js MQTT Client 기본 설정
    * MQTT Client를 위한 다음의 설정을 하였습니다.
        * .env 파일을 활용한 환경에 따른 클라이언트 정보 주입
        * MQTT 기반의 Nest.js 애플리케이션 설정
    
    Resolves: #2
    See also: None

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule);
+  await app.listen();
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    Logger.log(`Current environment mode: ${process.env.NODE_ENV}`);
+  })
+  .catch((error) => {
+    Logger.error(error);
+  });
