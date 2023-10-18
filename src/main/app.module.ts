b8749e9276631c64bab49aee1c118da61d628ea0
Mingi Kim <migni4575@gmail.com>
2023-10-18 23:35:30 +0900

    #10 feat: Http Client 구현
    Resolves: #10
    See also: None

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -1,16 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import MqttService from './mqtt.service';
 import { ConfigModule } from '@nestjs/config';
 import * as process from 'process';
+import { HttpModule } from '@nestjs/axios';
+import HttpClientService from '@main/http-client.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `config/.env.${process.env.NODE_ENV}`,
     }),
+    HttpModule.register({
+      baseURL: process.env.MAIN_SERVER,
+    }),
+    ScheduleModule.forRoot(),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [MqttService, HttpClientService],
 })
 export class AppModule {}
--- src/test/csv.parser.spec.ts
+++ src/test/csv.parser.spec.ts
@@ -3,7 +3,7 @@ import csvParser from '@main/csv.parser';
 describe('CSV Parser', () => {
   it('Parsing csv file', async () => {
     const records = await csvParser('config/test.csv');
-    console.log(records.length);
+
     expect(records.length).not.toBe(0);
   });
 });
