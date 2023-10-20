6a2c088abfdf427c61e3e8a6e82bd39fe6e71bcc
Mingi Kim <migni4575@gmail.com>
2023-10-20 17:33:57 +0900

    #14 feat: MQTT Client 데이터 전송 구현
    * MQTT Client가 브로커에 접속하여 일정 시간마다 데이터를 브로커에 전달합니다.
    
    Resolves: #14
    See also: None

--- src/main/csv.parser.ts
+++ src/main/csv.parser.ts
@@ -1,13 +1,33 @@
 import * as fs from 'fs';
 import { parse } from 'csv-parse';
 
-export default async (filename: string) => {
-  const records = [];
-  const parser = fs.createReadStream(filename).pipe(parse());
+export default class CsvParser {
+  static async csvFileToRecords(filename: string) {
+    const records = [];
+    const parser = fs.createReadStream(filename).pipe(parse());
 
-  for await (const record of parser) {
-    records.push(record);
+    for await (const record of parser) {
+      records.push(record);
+    }
+
+    return records;
   }
 
-  return records;
-};
+  static recordToCsvData(record: any): string {
+    return [
+      record[1],
+      record[2],
+      new Date(record[3]),
+      Number(record[4]),
+      Number(record[5]),
+      Number(record[6]),
+      Number(record[7]),
+      Number(record[8]),
+      Number(record[9]),
+      record[10],
+      record[11],
+      record[12],
+      record[13],
+    ].join();
+  }
+}
